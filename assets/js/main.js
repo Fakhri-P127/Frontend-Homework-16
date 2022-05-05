9da218560313904b177c7d45971ceb3a2c64fe1f
Fakhri-P127 <fakhrita@code.edu.az>
2022-05-06 02:43:39 +0400

    added Task-Santa folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,48 +2,67 @@ const submit = document.querySelector("#btnSubmit");
 const inputText = document.querySelector("#inputText");
 const ulList = document.querySelector("#ulList");
 const btnMatch = document.querySelector("#btnMatch");
-const msg = document.querySelector("#msg");
-const arr = [
-  "Fakhri",
-  "Avaz",
-  "Jahangir",
-  "Kamran",
-  "Ali",
-  "Lutfiyar",
-  "Ismayil",
-  "Isa",
-  "Gunel",
-  "Nigar",
-  "Aliv2",
-  "Mahammad",
-  "Ramin",
-  "Allahshukur",
-  "Aykhan",
-  "Oruc",
-  "Elvin",
-  "Sabir",
-  "Nariman",
-  "Parvin",
-  "Rabil",
-  "Mammad",
-];
+const sameName = document.querySelector("#sameName");
+const moreUsers = document.querySelector("#moreUsers");
+const divMatch = document.querySelector("#divMatch");
+const matchList = document.querySelector("#matchList");
+const cardMatch = document.querySelector("#cardMatch");
+let counter = 1;
+
+//For non dymanic input
+// const arr = [
+//   "Fakhri",
+//   "Avaz",
+//   "Jahangir",
+//   "Kamran",
+//   "Ali",
+//   "Lutfiyar",
+//   "Ismayil",
+//   "Isa",
+//   "Gunel",
+//   "Nigar",
+//   "Aliv2",
+//   "Mahammad",
+//   "Ramin",
+//   "Allahshukur",
+//   "Aykhan",
+//   "Oruc",
+//   "Elvin",
+//   "Sabir",
+//   "Nariman",
+//   "Parvin",
+//   "Rabil",
+//   "Mammad",
+// ];
 
 submit.addEventListener("click", function (e) {
   e.preventDefault();
-
-  //   const regex = /[\s]/g;
+  let sameInput = false;
 
   if (!inputText.value) {
     return;
   }
-  msg.classList.add("hidden");
-  const li = document.createElement("li");
-  ulList.appendChild(li);
-  li.innerHTML = `${`<i class="fa-regular fa-trash-can"></i>`} ${
-    inputText.value
-  }`;
-  inputText.value = "";
-  li.classList.add("custom-li");
+  if (ulList.children.length > 0) {
+    for (let i = 0; i < ulList.children.length; i++) {
+      if (inputText.value === ulList.children[i].textContent) {
+        sameInput = true;
+        sameName.classList.remove("hidden");
+        inputText.value = "";
+        return;
+      }
+    }
+  }
+
+  if (!sameInput) {
+    sameName.classList.add("hidden");
+    moreUsers.classList.add("hidden");
+    const li = document.createElement("li");
+
+    li.innerHTML = `<i class="fa-regular fa-face-frown"></i>${inputText.value}`;
+    li.classList.add("custom-li");
+    ulList.appendChild(li);
+    inputText.value = "";
+  }
 });
 
 btnMatch.addEventListener("click", function (e) {
@@ -65,10 +84,12 @@ btnMatch.addEventListener("click", function (e) {
   //#endregion
 
   if (ulList.children.length < 2) {
-    msg.classList.remove("hidden");
-    msg.classList.add("error");
+    moreUsers.classList.remove("hidden");
     return;
   }
+
+  divMatch.classList.remove("hidden");
+  cardMatch.classList.add("match-style");
 
   let firstMatch, secondMatch;
   do {
@@ -76,14 +97,17 @@ btnMatch.addEventListener("click", function (e) {
     firstMatch = ulList.children[randomNumber].textContent;
     randomNumber = Math.trunc(Math.random() * ulList.children.length);
     secondMatch = ulList.children[randomNumber].textContent;
-
-    if (firstMatch === secondMatch) {
-      msg.classList.remove("hidden");
-      msg.classList.add("error");
-      return;
-    }
-    console.log(
-      `${firstMatch} and ${secondMatch} matched, Go buy a gift ${firstMatch}!`
-    );
   } while (firstMatch === secondMatch);
+
+  const li = document.createElement("li");
+  li.innerHTML = `<span id="counterId">Match ${counter}:</span><i class="fa-regular fa-face-smile"></i> ${firstMatch} and ${secondMatch} matched, go buy a gift ${firstMatch}!`;
+
+  li.classList.add("match-li");
+  matchList.appendChild(li);
+
+  if (document.querySelector("#counterId")) {
+    const counterId = document.querySelectorAll("#counterId");
+    counterId.forEach((x) => x.classList.add("custom-span"));
+  }
+  counter++;
 });
